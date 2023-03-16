a761a16c78990a4d072c3514373b85a7e5cd010e
Quân The Immortal <chanvaicachan@gmail.com>
2023-03-16 10:44:05 +0700

    update 3/16

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import HomeHeader from "../../HomePage/HomeHeader";
 import './DetailDoctor.scss';
 import { getDetailInforDoctor } from "../../../services/userService";
-import { LANGUAGE, LANGUAGES } from "../../../utils";
+import { LANGUAGES } from "../../../utils";
+import DoctorSchedule from './DoctorSchedule';
 
 class DetailDoctor extends Component {
 
@@ -70,7 +71,14 @@ class DetailDoctor extends Component {
                     </div>
                 </div>
                 <div className="schedule-doctor">
-
+                    <div className="content-left">
+                        <DoctorSchedule
+                            doctorIdFromParent = {detailDoctor && detailDoctor.id ? detailDoctor.id : -1}
+                        />
+                        <div className="content-right">
+                            
+                        </div>
+                    </div>
                 </div>
                 <div className="detail-infor-doctor">
                     {detailDoctor && detailDoctor.Markdown && detailDoctor.Markdown.contentHTML
--- src/services/userService.js
+++ src/services/userService.js
@@ -45,12 +45,16 @@ const saveDetailDoctorService = (data) =>  {
     return axios.post('/api/save-infor-doctors', data)
 }
 
-const getDetailInforDoctor = (input) => {
+const getDetailInforDoctor = (inputId) => {
     return axios.get(`/api/get-detail-doctor-by-id?id=${inputId}`)
 }
 
 const saveBulkScheduleDoctor = (data) => {
     return axios.post('api/bulk-create-schedule', data)
+}
+
+const getScheduleDoctorByDate = (doctorId, date) => {
+    return axios.get(`/api/get-schedule-doctor-by-date?doctorId=${doctorId}&date=${date}`)
 }
 export { handleLoginApi,
     getAllUsers,
@@ -59,4 +63,5 @@ export { handleLoginApi,
     editUserService, getAllCodeService,
     getTopDoctorHomeService, getAllDoctors, saveDetailDoctorService,
     getDetailInforDoctor, saveBulkScheduleDoctor,
+    getScheduleDoctorByDate,
 }
